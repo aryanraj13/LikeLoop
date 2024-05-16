ae1bd65cce53a4c0e68a037365db861feea303a0
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-05-16 19:52:02 +0530

    Created Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -8,7 +8,8 @@ import {
     MenuItem,
     FormControl,
     useTheme,
-    useMediaQuery
+    useMediaQuery,
+    Input
 } from "@mui/material";
 import {
     Search,
@@ -33,8 +34,149 @@ const Navbar = () =>{
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
     const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
 
-    return (<div>Navbar</div>);
+    const fullname = "Aryan Rajput";
+    //`${user.firstName} ${user.lastName}`
+
+    return <FlexBetween padding = "1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap = "1.75rem">
+            <Typography 
+            fontWeight = "bold"
+            fontSize="clamp(1rem,2rem,2.25rem)"
+            color="primary"
+            onClick={()=>navigate("/home")}
+            sx={{
+                "&:hover":{
+                    color:primaryLight,
+                    cursor:"pointer",
+                },
+            }}
+            >
+                LookLike
+            </Typography>
+            {isNonMobileScreens && (
+                <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+                    <InputBase placeholder="Search..."/>
+                    <IconButton>
+                        <Search/>
+                    </IconButton>
+                </FlexBetween>
+            )}
+        </FlexBetween>
+        {/*DESKTOP NAV*/}
+        {isNonMobileScreens?(
+            <FlexBetween gap = "2rem">
+                <IconButton onClick={()=>dispatch(setMode())}>
+                    {theme.palette.mode==="dark"?(
+                        <DarkMode sx = {{fontSize:"25px"}}/>
+                    ):(
+                        <LightMode sx= {{color:dark,fontSize:"25px"}}/>
+                    )}
+                </IconButton>
+                <Message sx = {{fontSize:"25px"}}/>
+                <Notifications sx = {{fontSize:"25px"}}/>
+                <Help sx = {{fontSize:"25px"}}/>
+                <FormControl variant = "standard" value={fullname}>
+                    <Select
+                    value = {fullname}
+                    sx={{
+                        backgroundColor:neutralLight,
+                        width:"150px",
+                        borderRadius :"0.25rem",
+                        p:"0.25rem 1rem",
+                        "& .MuiSvgIcon-root":{
+                            pr:"0.25rem",
+                            width:"3rem"
+                        },
+                        "& .MuiSelect-select:focus":{
+                            backgroundColor:neutralLight,
+                        },
+                    }}
+                    input={<InputBase/>}
+                    >
+                    <MenuItem value={fullname}>
+                        <Typography>{fullname}</Typography>
+                    </MenuItem>
+                    <MenuItem onClick={()=>dispatch(setLogout())}>Log Out</MenuItem>
+                    </Select>
+                </FormControl>
+            </FlexBetween>
+        ):(
+            <IconButton
+            onClick={()=>setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+                <Menu/>
+            </IconButton>
+        )}
+
+        {/*Mobile nav*/}
+        {!isNonMobileScreens && isMobileMenuToggled &&(
+            <Box
+            position="fixed"
+            right="0"
+            bottom="0"
+            height="100%"
+            zIndex="10"
+            maxWidth="500px"
+            minWidth="300px"
+            backgroundColor={background}
+            >
+                    {/* CLOSE ICON */}
+                    <Box display="flex" justifyContent="flex-end" p="1rem">
+                        <IconButton
+                        onClick={()=>setIsMobileMenuToggled(!isMobileMenuToggled)}
+                        >
+                            <Close/>
+                        </IconButton>
+                    </Box>
+
+                    {/*Menu Item*/}
+                    <FlexBetween display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap = "3rem">
+                <IconButton onClick={()=>dispatch(setMode())}
+                sx = {{fontSize:"25px"}}
+                >
+                    {theme.palette.mode==="dark"?(
+                        <DarkMode sx = {{fontSize:"25px"}}/>
+                    ):(
+                        <LightMode sx= {{color:dark,fontSize:"25px"}}/>
+                    )}
+                </IconButton>
+                <Message sx = {{fontSize:"25px"}}/>
+                <Notifications sx = {{fontSize:"25px"}}/>
+                <Help sx = {{fontSize:"25px"}}/>
+                <FormControl variant = "standard" value={fullname}>
+                    <Select
+                    value = {fullname}
+                    sx={{
+                        backgroundColor:neutralLight,
+                        width:"150px",
+                        borderRadius :"0.25rem",
+                        p:"0.25rem 1rem",
+                        "& .MuiSvgIcon-root":{
+                            pr:"0.25rem",
+                            width:"3rem"
+                        },
+                        "& .MuiSelect-select:focus":{
+                            backgroundColor:neutralLight,
+                        },
+                    }}
+                    input={<InputBase/>}
+                    >
+                    <MenuItem value={fullname}>
+                        <Typography>{fullname}</Typography>
+                    </MenuItem>
+                    <MenuItem onClick={()=>dispatch(setLogout())}>Log Out</MenuItem>
+                    </Select>
+                </FormControl>
+            </FlexBetween>
+            </Box>
+        )} 
+    </FlexBetween>;
 }
 
 export default Navbar;
